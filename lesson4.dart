08fbf75303227fd9b80755485adf34c41bd0a2cd
marlenkv <kumarbekovmarlen@gmail.com>
2022-10-12 19:29:31 +0600

    first commit

---
+++ lesson4.dart
@@ -0,0 +1,28 @@
+void main() {
+  // String outPut;
+  // int a = 30;
+  // outPut = a > 29 ? "Значение больше 30" : "Значение меньше 30";
+  // print(outPut);
+
+  String name = "marlen";
+  if (name.contains("m")) {
+    print("имя содержит букву м");
+  }
+  if (name.contains("a")) {
+    print("имя содержит букву а");
+  }
+  if (name.contains("r")) {
+    print("имя содержит букву r");
+  }
+  if (name.contains("l")) {
+    print("имя содержит букву l");
+  }
+  if (name.contains("e")) {
+    print("имя содержит букву e");
+  }
+  if (name.contains("n")) {
+    print("имя содержит букву n");
+  } else {
+    print("имя не содержит ни одну букву");
+  }
+}
